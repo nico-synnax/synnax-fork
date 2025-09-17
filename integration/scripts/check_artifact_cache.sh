85ed80f4a83738ed737c2dd0d816f31dc9132537
nico-synnax <nico@synnaxlabs.com>
2025-09-16 21:49:28 -0500

    Linting

--- integration/scripts/check_artifact_cache.sh
+++ integration/scripts/check_artifact_cache.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Script to check for cached artifacts and download them if available
+# Copyright 2025 Synnax Labs, Inc.
+#
+# Use of this software is governed by the Business Source License included in the file
+# licenses/BSL.txt.
+#
+# As of the Change Date specified in that file, in accordance with the Business Source
+# License, use of this software will be governed by the Apache License, Version 2.0,
+# included in the file licenses/APL.txt.
 
 set -e
 
